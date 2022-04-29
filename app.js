80db17f1c0cffee4e481ca6e6c56f374fdddbcf4
Felix Chen <fechen1993@gmail.com>
2022-04-29 10:52:03 -0400

    add more game functionality

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const WINNING_COMBINATION = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
 const cellElements = document.querySelectorAll('[data-cell');
 const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn;
 
@@ -23,34 +25,52 @@ cellElements.forEach(cell => {
 
 startGame();
 
+restartButton.addEventListener('click', startGame)
+
 function startGame() {
     circleTurn = false;
     cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick);
         cell.addEventListener('click', handleClick, { once: true })
     })
     setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+
 }
 
 function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
     placeMark(cell, currentClass);
-    if(checkWin(currentClass)) {
+    if (checkWin(currentClass)) {
         endGame(false);
     }
-    swapTurns();
-    setBoardHoverClass();
+    else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+
 }
 
 function endGame(draw) {
-    if(draw) {
-
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
     }
     else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's win" : "X's"} win`
+        winningMessageTextElement.innerText = `${circleTurn ? "O's win" : "X's"} win`;
     }
+    winningMessageElement.classList.add('show');
 }
 
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
+}
 
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass);
